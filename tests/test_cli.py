ad0c0864e0d882f99c701c3d6481340f0ca06e97
Roy Moore <roy@moore.co.il>
2024-01-09 11:29:30 +0200

    Update CLI test to inlcude all types

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,10 +1,12 @@
 """Test CLI."""
+from string import Template
+
 from click.testing import CliRunner
-from pytest import LogCaptureFixture, MonkeyPatch
+from pytest import LogCaptureFixture, MonkeyPatch, mark
 
 from semvergit.cli import cli
 
-UPDATE_VERSION = "1.1.2"
+BUMP_STRING = Template("<used $bump_type>")
 
 
 class MockSemverGit:  # pylint: disable=too-few-public-methods
@@ -12,13 +14,14 @@ class MockSemverGit:  # pylint: disable=too-few-public-methods
 
     def update(self, bump_type: str) -> str:  # pylint: disable=unused-argument
         """Update."""
-        return UPDATE_VERSION
+        return BUMP_STRING.substitute(bump_type=bump_type)
 
 
-def test_cli(caplog: LogCaptureFixture) -> None:
+@mark.parametrize("bump_type", ["major", "minor", "patch", "prerelease"])
+def test_cli(caplog: LogCaptureFixture, bump_type: str) -> None:
     """Test CLI."""
     runner = CliRunner()
     MonkeyPatch().setattr("semvergit.cli.SemverGit", MockSemverGit)
-    result = runner.invoke(cli, ["--bump_type", "patch"])
+    result = runner.invoke(cli, ["--bump_type", bump_type])
     assert result.exit_code == 0
-    assert caplog.messages == [f"New version: {UPDATE_VERSION}"]
+    assert caplog.messages == [f"New version: {BUMP_STRING.substitute(bump_type=bump_type)}"]
