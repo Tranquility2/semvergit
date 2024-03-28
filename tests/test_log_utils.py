ebc5136fd7db1107a357c076a2de462d6f993a8e
Roy Moore <roy@moore.co.il>
2024-03-28 10:17:54 +0200

    fix type

--- tests/test_log_utils.py
+++ tests/test_log_utils.py
@@ -6,7 +6,7 @@ from semvergit.log_utils import set_logger
 def test_set_logger_invalid() -> None:
     """Test set_logger with invalid mode."""
     try:
-        set_logger("invalid")
+        set_logger("invalid")  # type: ignore
     except ValueError:
         pass
     else:
